c93883f4edafa2e95a6817c13c1f13af5540f082
Stamatios-Korres <timoskorres@hotmail.com>
2019-03-11 23:12:35 +0000

    synced the machines

--- Exercise3/Worker.py
+++ Exercise3/Worker.py
@@ -2,23 +2,46 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-
+from sys import maxsize
 from Networks import ValueNetwork
 from torch.autograd import Variable
 from Environment import HFOEnv
 import random
 
+def init_environment(idx):
+        hfoEnv = HFOEnv(numTeammates=0, numOpponents=1, port=6000+idx, seed=idx)
+        hfoEnv.connectToServer()
+        return hfoEnv
+
 def train():
+        pass
 
 def computeTargets(reward, nextObservation, discountFactor, done, targetNetwork):
+        pass
 
 def computePrediction(state, action, valueNetwork):
-	
+        pass
+
 # Function to save parameters of a neural network in pytorch.
 def saveModelNetwork(model, strDirectory):
-	torch.save(model.state_dict(), strDirectory)
-	
+        torch.save(model.state_dict(), strDirectory)
 
+# Set the target parameters to be exactly the same as the source
+def hard_update(target, source):
+        for target_param, param in zip(target.parameters(), source.parameters()):
+                target_param.data.copy_(param.data)
 
+def implement_actor_critic(idx, target_value_network, optimizer, lock, counter):
+        discount_factor = 0.9
+        hfoEnv = init_environment(idx)
+        episodeNumber = 0
+        observation = hfoEnv.reset()
+        while True:
+                policy,stateValue = target_value_network(observation)
+                act = hfoEnv.act(action)
+                newObservation, reward, done, status, info = hfoEnv.step(act)
+                print(newObservation, reward, done, status, info)
 
+                if done:
+                        episodeNumber += 1
 
--- Exercise3/main.py
+++ Exercise3/main.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
 # encoding utf-8
 from Environment import HFOEnv
+import torch
 import torch.multiprocessing as mp
 from Networks import ValueNetwork
+from Worker import train
+import argparse
+from SharedAdam import SharedAdam
+import copy
+
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                    help='SGD momentum (default: 0.5)')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--num_processes', type=int, default=2, metavar='N',
+                    help='how many training processes to use (default: 2)')
+parser.add_argument('--cuda', action='store_true', default=False,
+                    help='enables CUDA training')
+
 
 # Use this script to handle arguments and 
 # initialize important components of your experiment.
@@ -10,23 +32,30 @@ from Networks import ValueNetwork
 # your models, torch's multiprocessing methods, etc.
 
 if __name__ == "__main__" :
-	
-	# Example on how to initialize global locks for processes
-	# and counters.
-	env = HFOEnv()
-	env.connectToServer()
-	env.startEnv()
-	print("State returns:", env.act('MOVE'))
-	counter = mp.Value('i', 0)
-	lock = mp.Lock()
-	processes = []
-	for idx in range(0, args.num_processes):
-		trainingArgs = (idx, args, value_network, target_value_network, optimizer, lock, counter)
-		p = mp.Process(target=train, args=())
-		p.start()
-		processes.append(p)
-	for p in processes:
-		p.join()
+
+        # Example on how to initialize global locks for processes
+        # and counters.
+        numOpponents = 1
+
+        args = parser.parse_args()
+        counter = mp.Value('i', 0)
+        lock = mp.Lock()
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # Initialize shared network
+
+        val_network = ValueNetwork(state_size=15,action_size=4,hidden_dimension=64)
+        val_network.apply(val_network.init_weights)
+        optimizer = SharedAdam(params=val_network.parameters())
+        val_network.share_memory()
 
 
+        processes = []
+        for idx in range(0, args.num_processes):
+                trainingArgs = (idx, val_network, optimizer, lock, counter)
+                p = mp.Process(target=train, args=trainingArgs)
+                p.start()
+                processes.append(p)
+        for p in processes:
+                p.join()
 
